d035a2cf27761c79cd66f52e364ad6fd7a20a59f
Felix Buehler <account@buehler.rocks>
2024-07-04 21:58:01 +0200

    ignore dependency conflicts

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,4 +2,5 @@
 set -e
 cd /opt/pkgdir
 DEPS=$(cat PKGBUILD | grep depends | awk -F'[()]' '{print $2}' | sed 's/:.*"/"/' | tr '\n' ' ' | tr -d "'" | tr -d '"')
-yay -S $DEPS --noconfirm
+yay -S $DEPS --noconfirm --overwrite '*'
+
